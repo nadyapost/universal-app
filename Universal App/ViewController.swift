84de5c5eb6fcf6ebd40da74ac22ca4a9346ad67b
nadyapost <nadyapostr@gmail.com>
2019-08-17 19:29:05 +1000

    Initial Commit

---
+++ Universal App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Universal App
+//
+//  Created by Nadya Postriganova on 17/8/19.
+//  Copyright © 2019 Nadya Postriganova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
