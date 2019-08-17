18f9ecd490ad00134cf93fe8f8469515fd3a17ed
nadyapost <nadyapostr@gmail.com>
2019-08-17 20:23:23 +1000

    Add and link custom cell class, add movie model

---
+++ Universal App/Models/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  Universal App
+//
+//  Created by Nadya Postriganova on 17/8/19.
+//  Copyright © 2019 Nadya Postriganova. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+  let title: String
+  let imageHref: String
+  let rating: Double
+  let releaseDate: String
+}
---
+++ Universal App/Views/CustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCell.swift
+//  Universal App
+//
+//  Created by Nadya Postriganova on 17/8/19.
+//  Copyright © 2019 Nadya Postriganova. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+  @IBOutlet var movieImage: UIImageView!
+  @IBOutlet var label: UILabel!
+  
+
+}
